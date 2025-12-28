c1fdc605a7bcb2b6dc3bd2cbbb253f362100bbfc
Davide DR <d.dr@libero.it>
2025-12-28 19:57:14 +0100

    Completes part 0.a.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -22,12 +22,20 @@ const App = () => {
 
   const name = "Peter"
   const age = 18
+
+  const friends = [{ name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
   return (
     <>
       <h1>Greetings!</h1>
       <Hello name="John" age={26+10}/>
       <Hello name="Jane" age={age}/>
       <Hello name={name} age={age}/>
+
+      <Hello name={friends[0].name} age={friends[0].age}/>
+      <Hello name={friends[1].name} age={friends[1].age}/>
       <Footer />
     </>
   )
