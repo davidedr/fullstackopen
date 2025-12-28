a86c982c1379da9e158410beeaeece6713c9b241
Davide DR <d.dr@libero.it>
2025-12-28 19:36:20 +0100

    Completes part 0.a.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,22 @@
-function App() {
+ const Hello = () => {
+  return(
+    <div><p>Hello, world!</p></div>
+  )
+ }
+
+const App = () => {
   console.log("Hello from component!");
+  
   const now = Date();
   const a = 10;
   const b = 20;
   console.log(now, a + b);
   return (
     <div>
-      <p>Hello, world, it is {now.toString()}</p>
-      <p>{a} plus {b} is {a + b}.</p>
+      <h1>Greetings!</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
