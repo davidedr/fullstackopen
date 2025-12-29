5d76529ba9d310eaf1eb35b9da90c0871ff76cc0
Davide DR <d.dr@libero.it>
2025-12-29 22:52:53 +0100

    Handles complex state

--- study projects/partd-complex-state/src/App.jsx
+++ study projects/partd-complex-state/src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clicks, setClicks] = useState({ left: 0, right: 0});
 
+  // However, it is forbidden in React to mutate state directly,
+  // since it can result in unexpected side effects.
+  // Changing state has to always be done by setting the state to a new object.
+  // If properties from the previous state object are not changed,
+  // they need to simply be copied, which is done by copying those properties into a new object
+  // and setting that as the new state.
   return (
     <>
+      <div>{clicks.left}, {clicks.right}</div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => {setClicks({...clicks, left: clicks.left + 1})}}>Left</button>
+        <button onClick={() => {setClicks({...clicks, right: clicks.right + 1})}}>Right</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
