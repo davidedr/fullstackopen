3fd9bbab6c1dbdf474f2b44b06343cab3833082d
Davide DR <d.dr@libero.it>
2025-12-29 12:29:09 +0100

    Adds component helper function

--- study projects/partc/src/App.jsx
+++ study projects/partc/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = (props) => {
+  
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  }
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  return(
+    <div>
+      <p>Hello {props.name}, you're {props.age} years old</p>
+      <p>So, you're born in {bornYear()}.</p>
+    </div>
+  );
+}
+const App = () => {
+  const name = "Peter"
+  const age = 18
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Greetings</h1>
+        <Hello name="Maya" age={26+10} />
+        <Hello name={name} age={age} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
