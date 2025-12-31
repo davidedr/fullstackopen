4e4b1b6afb993b8b6392a349d66db4d6a5f06dbb
Davide DR <d.dr@libero.it>
2025-12-31 21:43:07 +0100

    Completes part2 a Modules refactoring

--- study projects/part2a-rendering-collections/src/App.jsx
+++ study projects/part2a-rendering-collections/src/App.jsx
@@ -1,11 +1,12 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
 
+// Note that the key attribute must now be defined for the Note components, and not for the li tags like before.
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note => <Note key={note.id} note={note}/>)}
       </ul>
     </div>
   )
