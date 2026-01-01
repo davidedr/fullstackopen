e0340d3519d2d862749b653731d69bc1dd78c97b
Davide DR <d.dr@libero.it>
2026-01-01 14:28:58 +0100

    Completes 2.3*: Course information step 8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,8 +1,17 @@
-const Course = ({ course}) => {
+const Course = ({ course }) => {
 
   const sumExerceises = (parts) => {
     let sumExercises = 0;
     parts.map(part => sumExercises += part.exercises)
+    return sumExercises
+  }
+
+  const sumExerceisesReduce = (parts) => {
+    let acc = 0;
+    const sumExercises = parts.reduce((acc, part) => {
+      console.log(1, part.id, part.exercises, acc)
+      return acc + part.exercises;
+    }, 0);
     return sumExercises
   }
 
@@ -12,7 +21,8 @@ const Course = ({ course}) => {
     <ul>
       {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
     </ul>
-    <b>total of {sumExerceises(course.parts)} excercises</b>
+    {/* <b>total of {sumExerceisesReduce(course.parts)} excercises</b> <br /> */}
+    <b>total of {course.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)} excercises</b>
     </>
   )
 }
