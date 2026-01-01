6751f0b34da2588fe5d66f23990d643d8f4b4d2b
Davide DR <d.dr@libero.it>
2026-01-01 16:55:00 +0100

    Completes 2.6: The Phonebook Step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,52 @@
 import { useState } from 'react'
 
+const Numbers = ({ persons }) => {
+  console.log("Numbers", persons.length, persons);
+  
+  if (persons.length === 0)
+    return (<p>No numbers yet.</p>)
+  else
+    return (
+      <>
+        <h2>Numbers</h2>
+        <ul>
+          {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        </ul>
+      </>
+    )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  ])
+  const [newName, setNewName] = useState('Add a name...')
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const newPersonObj = { name: newName }
+    //setPersons([...persons, newPersonObj])
+    setPersons(persons.concat(newPersonObj))
+    setNewName("")
+  }
+
+  const handleNameChange = (event) => {
+    console.log("handleNameChange", event.target, event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <Numbers persons={persons} />
     </div>
   )
 }
