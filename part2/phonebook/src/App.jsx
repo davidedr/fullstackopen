a52d1dd686a1c42834e170dd4ada70fe126d828a
Davide DR <d.dr@libero.it>
2026-01-01 18:40:30 +0100

    Completes 2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Numbers = ({ persons }) => {
   console.log("Numbers", persons.length, persons);
-  
+
   if (persons.length === 0)
     return (<p>No numbers yet.</p>)
   else
@@ -18,21 +18,48 @@ const Numbers = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('Add a name...')
   const [newNumber, setNewNumber] = useState('Add a number...')
+  const [filteredPersons, setFilteredPersons] = useState([...persons])
+  const [newFilter, setNewFilter] = useState('');
+
+  const makeFiltering = (newFilter, newPerson = null) => {
+    console.log("makeFiltering", newFilter);
+    const newPersons = [...persons]
+    if (newPerson != null)
+      newPersons.push(newPerson)
+    
+    if (newFilter === "")
+      setFilteredPersons([...newPersons])
+    else
+      setFilteredPersons(newPersons.filter(
+        person => person.name.toLowerCase().includes(newFilter.toLowerCase())
+      ))
+  }
+
+  const handleFilterChange = (event) => {
+    const newFilter = event.target.value;
+    setNewFilter(newFilter)
+    makeFiltering(newFilter)
+  }
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const newPersonObj = { name: newName, number: newNumber, id: persons.length + 1 }
     if (persons.filter(person => person.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
+    const newPersonObj = { name: newName, number: newNumber, id: persons.length + 1 }
     setPersons(persons.concat(newPersonObj))
     setNewName("")
     setNewNumber("")
+    makeFiltering(newFilter, newPersonObj)
   }
 
   const handleNameChange = (event) => {
@@ -40,23 +67,24 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-    const handleNumberChange = (event) => {
+  const handleNumberChange = (event) => {
     console.log("handleNumberChange", event.target, event.target.value)
     setNewNumber(event.target.value)
   }
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with<input value={newFilter} onChange={handleFilterChange}></input></p>
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers persons={persons} />
+      <Numbers persons={filteredPersons} />
     </div>
   )
 }
