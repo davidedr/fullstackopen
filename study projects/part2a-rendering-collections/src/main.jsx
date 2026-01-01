87f6725d7c7cf865d8a4d425a39f244f0b2edcf5
Davide DR <d.dr@libero.it>
2026-01-01 20:41:48 +0100

    Completes part 2 c Axios and promises

--- study projects/part2a-rendering-collections/src/main.jsx
+++ study projects/part2a-rendering-collections/src/main.jsx
@@ -19,6 +19,20 @@ const notes = [
   }
 ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+import axios from 'axios'
+
+/*
+const promise = axios.get('http://localhost:3001/notes')
+                  .then(res => console.log(res));
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+*/
+axios.get('http://localhost:3001/notes')
+  .then(res => {
+    const notes = res.data;
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <App notes={notes} />
+    )
+  })
