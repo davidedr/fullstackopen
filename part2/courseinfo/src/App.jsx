d38fd884637e1785efb12c2d07f1374385b3e457
Davide DR <d.dr@libero.it>
2026-01-01 14:48:44 +0100

    Completes 2.5: Separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,33 +1,7 @@
-const Course = ({ course }) => {
-
-  const sumExerceises = (parts) => {
-    let sumExercises = 0;
-    parts.map(part => sumExercises += part.exercises)
-    return sumExercises
-  }
-
-  const sumExerceisesReduce = (parts) => {
-    let acc = 0;
-    const sumExercises = parts.reduce((acc, part) => {
-      console.log(1, part.id, part.exercises, acc)
-      return acc + part.exercises;
-    }, 0);
-    return sumExercises
-  }
-
-  return(
-    <>
-    <h1>{course.name}</h1>
-    <ul>
-      {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
-    </ul>
-    {/* <b>total of {sumExerceisesReduce(course.parts)} excercises</b> <br /> */}
-    <b>total of {course.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)} excercises</b>
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
+  
   const courses = [
     {
       name: 'Half Stack application development',
@@ -72,7 +46,7 @@ const App = () => {
       ]
     }
   ]
-  
+
   return courses.map(course => <Course key={course.id} course={course} />)
 }
 
