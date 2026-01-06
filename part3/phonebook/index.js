b77245ac114c747f330fc2c6f858bf37de9307c3
Davide DR <d.dr@libero.it>
2026-01-06 15:35:19 +0100

    Completes 3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -21,6 +21,8 @@ let persons = [
     }
 ]
 
+const MAX_ID=1E10
+
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -35,9 +37,9 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
     if (!req.params)
-        return res.status(404).json({ error: "Not valid"})
+        return res.status(400).json({ error: "Not valid"})
     if (!req.params.id)
-        return res.status(404).json({ error: "Not valid"})
+        return res.status(400).json({ error: "Not valid"})
     const person = persons.filter(person => person.id === req.params.id)
     if (!person || person.length === 0)
         return res.status(404).json({ error: "Not found"})
@@ -46,9 +48,9 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     if (!req.params)
-        return res.status(404).json({ error: "Not valid"})
+        return res.status(400).json({ error: "Not valid"})
     if (!req.params.id)
-        return res.status(404).json({ error: "Not valid"})
+        return res.status(400).json({ error: "Not valid"})
     const person = persons.filter(person => person.id === req.params.id)
     if (!person || person.length === 0)
         return res.status(404).json({ error: "Not found"})
@@ -56,6 +58,19 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(202).end()
 })
 
+app.post('/api/persons', (req, res) => {
+    console.log('POST /api/persons');
+    console.log(req.body);
+       
+    
+    if (!req.body)
+        return res.status(400).json({ error: "Not valid"})
+    const person = {
+        id: String(Math.floor(Math.random()*MAX_ID)), name: req.body.name, number: req.body.number
+    }
+    persons = [...persons, person]
+    res.json(person)
+})
 
 PORT = 3001
 app.listen(PORT, () => console.log(`Server listens to port ${PORT}`))
